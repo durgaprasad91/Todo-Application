09448786ff6a2826a8158214b993a30f219c1421
durgaprasad91 <durga00312@gmail.com>
2024-07-13 21:01:31 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import TodoList from './components/TodoList';
-import './index.css'
+import './index.css';
+
+// Lazy load the TodoList component
+const TodoList = lazy(() => import('./components/TodoList.js'));
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoList />
+    <Suspense fallback={<div>Loading...</div>}>
+      <TodoList />
+    </Suspense>
   </Provider>,
   document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[name].[contenthash].chunk.js',
   },
   module: {
     rules: [
@@ -22,9 +26,21 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html', 
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // Change to 'server' to run a local server for the analysis
+      openAnalyzer: false, // Change to true to open the analysis report automatically
     }),
   ],
   devServer: {
